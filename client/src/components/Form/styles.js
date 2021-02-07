814c58bf411fbea2eb04c28d077730bc782e79e9
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-08 07:44:57 +1100

    Update styles.js

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -20,5 +20,9 @@ export default makeStyles((theme) => ({
   },
   buttonSubmit: {
     marginBottom: 10,
+    fontFamily: 'Montserrat'
+  },
+  textStyle: {
+    fontFamily: 'Montserrat'
   },
 }));
