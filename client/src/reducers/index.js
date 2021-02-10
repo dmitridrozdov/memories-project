2ffde8a9445855bc76a1bd9272dfbb8cfd41e23b
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-10 11:48:43 +1100

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,3 @@ import { combineReducers } from 'redux'
 import posts from './posts'
 
 export default combineReducers({ posts })
-
-// import { combineReducers } from 'redux';
-
-// import posts from './posts';
-
-// export const reducers = combineReducers({ posts });
