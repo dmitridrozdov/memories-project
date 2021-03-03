b03b33380024c39865d25cd4745759a3f1902299
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-03 11:47:45 +1100

    Create auth.js

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+//click the like button => auth middleware (NEXT) => like controller
+
+const auth = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const isCustomAuth = token.length < 500
+        let decodedData
+        if(token && isCustomAuth) {
+            decodedData = jwt.verify(token, 'test')
+            req.userId = decodedData?.id
+        } else {
+            decodedData = jwt.decode()
+            req.userId = decodedData?.sub
+        }
+        next()
+    } catch(error) {
+
+    }
+}
+
+export default auth
