70dd88b8192d593075b5d226d768d27a7382ac76
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-03 11:47:34 +1100

    Update auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,18 +1,23 @@
 import { AUTH } from '../constants/actionTypes'
 import * as api from '../api'
+import { useHistory } from 'react-router-dom'
+
+
 
 export const signin = (post) => async(dispatch) => {
     try {
         //login the user
+        const history = useHistory()
         history.push('/')
     } catch(error) {
         console.log(error)
     }   
 }
 
-export const signout = (formData, history) => async(dispatch) => {
+export const signup = (formData, history) => async(dispatch) => {
     try {
         //sign up the user
+        const history = useHistory()
         history.push('/')
     } catch(error) {
         console.log(error)
