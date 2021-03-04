015228f274dac692d738c782f5e657de21159e64
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-04 12:45:43 +1100

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -36,7 +36,7 @@ const Auth = () => {
 
     const switchMode = () => {
         setIsSignUp((prevIsSignup) => !prevIsSignup)
-        handleShowPassword(false)
+        setShowPassword(false)
     }
 
     const googleSuccess = async (res) => {
